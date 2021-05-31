bf5b865eeb9d768deffb5a26fe07f7fe804fa6b8
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-31 08:24:54 +0530

    Add : Addition method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 class HelloWorld {
 	public static void main(String args[]) {
-		System.out.println("Hello World");
+		System.out.println("Helloworld");		
+	}
+	public int add(int a, int b) {
+		return a+b;
 	}
 }
